d7922f81978b564fc21de2259ef7199ab805aacb
cadu <cadu.coelho@gmail.com>
2024-06-30 23:19:46 -0300

    Mongolinho Db

--- cunha_visivel/cli/__init__.py
+++ cunha_visivel/cli/__init__.py
@@ -1,59 +1,43 @@
 from pathlib import Path
 import click
 from loguru import logger
+import requests
 
 from cunha_visivel.scraping import CunhaScraper
-from cunha_visivel.utils.folder import create_workdir_from_path
+from cunha_visivel.utils.workdir import create_workdir_from_path
+from cunha_visivel.workdir.operator import WorkdirOperator
 
 
 # create a click cli command to download the pdfs
 @click.command()
 @click.option("--headful", is_flag=True, help="Run the browser in headful mode")
+@click.option(
+    "--at-most", type=int, default=10, help="Download at most this number of PDFs"
+)
 # add folder argument
 @click.argument("workdir_path", type=click.Path())
-def cunha_cli(headful: bool, workdir_path: Path) -> None:
+def cunha_cli(headful: bool, workdir_path: Path, at_most: int):
     # Ensure the folder exists and ends with ".workdir"
     workdir_path = Path(workdir_path).absolute()
-    workdir_path = create_workdir_from_path(workdir_path)
+    if ".workdir" not in workdir_path.suffix:
+        workdir_path = Path(str(workdir_path) + ".workdir").absolute()
+
+    if not workdir_path.exists():
+        workdir_path = create_workdir_from_path(workdir_path)
+
+    # Start Workdir Operator
+    workdir_op = WorkdirOperator(workdir_path)
 
     # Get all PDF links:
-    pdf_links = CunhaScraper(headful=headful).get_pdf_links()
+    pdf_links = CunhaScraper(headful=headful).get_pdf_links(at_most=at_most)
 
-    # chrome_options = webdriver.ChromeOptions()
+    for pdf_link in pdf_links:
+        if pdf_link in workdir_op:
+            logger.warning(f"PDF {pdf_link} exists in the workdir, skipping...")
+            continue
 
-    # if not headful:
-    #     chrome_options.add_argument("--headless")
+        workdir_op.download_pdf(pdf_link)
 
-    # chrome_options.add_experimental_option(
-    #     "prefs",
-    #     {
-    #         "download.default_directory": "./assets/pdf",
-    #         "download.prompt_for_download": False,
-    #         "download.directory_upgrade": True,
-    #         "safebrowsing.enabled": True,
-    #     },
-    # )
 
-    # driver = webdriver.Chrome(options=chrome_options)
-
-    # try:
-    #     driver.get("https://www.imprensaoficialmunicipal.com.br/cunha")
-
-    #     while True:
-    #         links = driver.find_elements(
-    #             By.CSS_SELECTOR, 'a[href^="https://dosp.com.br/impressao.php?i="]'
-    #         )
-    #         for link in links:
-    #             pdf_url = link.get_attribute("href")
-
-    #             if not already_downloaded(pdf_url):
-    #                 download_pdf(pdf_url, driver)
-    #                 extract_filename_from_url(pdf_url)
-
-    #         try:
-    #             next_button = driver.find_element(By.CSS_SELECTOR, "a.next")
-    #             next_button.click()
-    #         except NoSuchElementException:
-    #             break
-    # finally:
-    #     driver.quit()
+if __name__ == "__main__":
+    cunha_cli()
--- cunha_visivel/scraping/__init__.py
+++ cunha_visivel/scraping/__init__.py
@@ -1,9 +1,11 @@
 import random
+import shutil
 import time
 from loguru import logger
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 
 
 class CunhaScraper:
@@ -21,7 +23,7 @@ class CunhaScraper:
         self.pdf_links_css_selector = pdf_links_css_selector
         self.headful = headful
 
-    def get_pdf_links(self) -> list[str]:
+    def get_pdf_links(self, at_most: int = 0) -> list[str]:
         """
         Get all PDF download links from the Cunha Imprensa Oficial website.
         """
@@ -41,16 +43,20 @@ class CunhaScraper:
             },
         )
 
+        # Get the "chromedriver" executable from the PATH
         logger.info("Starting Chrome driver...")
-        driver = webdriver.Chrome(options=chrome_options)
+        chromedriver_path = shutil.which("chromedriver")
+        driver = webdriver.Chrome(
+            options=chrome_options, service=Service(executable_path=chromedriver_path)
+        )
         logger.info("Chrome driver started.")
 
         pdf_links = []
 
         try:
-            logger.info(f"Opening URL \"{self.cunha_imprensa_oficial_url}\"...")
+            logger.info(f'Opening URL "{self.cunha_imprensa_oficial_url}"...')
             driver.get(self.cunha_imprensa_oficial_url)
-            logger.info(f"Opened URL \"{self.cunha_imprensa_oficial_url}\".")
+            logger.info(f'Opened URL "{self.cunha_imprensa_oficial_url}".')
 
             while True:
                 logger.info("Getting PDF links...")
@@ -63,9 +69,13 @@ class CunhaScraper:
                     if not pdf_url in pdf_links:
                         logger.info(f"Adding PDF link: {pdf_url}")
                         pdf_links.append(pdf_url)
-
+                        if at_most > 0 and len(pdf_links) >= at_most:
+                            logger.info(f"Reached the limit of {at_most} PDF links.")
+                            break
+                if at_most > 0 and len(pdf_links) >= at_most:
+                    break
                 try:
-                    logger.info("Clicking \"next\" button in the webpage...")
+                    logger.info('Clicking "next" button in the webpage...')
                     next_button = driver.find_element(By.CSS_SELECTOR, "a.next")
                     # sleep for a random short time to avoid being blocked
                     time.sleep(random.uniform(0.5, 1.5))
